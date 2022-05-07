d31b6a4b4f489f407842416004d8daf7511ea69e
Leandro Rondon <lerondon@gmail.com>
2022-05-07 19:55:25 +0100

    Section 10 - Blog docker-compose file, proto file and  server boilerplate

---
+++ blog/server/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"net"
+
+	pb "github.com/leandrorondon/grpc-go-course/blog/proto"
+	"google.golang.org/grpc"
+)
+
+var collection *mongo.Collection
+var addr = "0.0.0.0:50051"
+
+type Server struct {
+	pb.BlogServiceServer
+}
+
+func main() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://root:root@localhost:27017/"))
+	if err != nil {
+		log.Fatalf("Failed to create mongo client: %v\n", err)
+	}
+
+	err = client.Connect(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to connect to mongo server: %v\n", err)
+	}
+
+	collection = client.Database("blogdb").Collection("blog")
+
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to listen on: %v\n", err)
+	}
+
+	log.Printf("Listening on %s\n", addr)
+
+	s := grpc.NewServer()
+	pb.RegisterBlogServiceServer(s, &Server{})
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v\n", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/leandrorondon/grpc-go-course
 go 1.18
 
 require (
+	go.mongodb.org/mongo-driver v1.9.1
 	google.golang.org/grpc v1.46.0
 	google.golang.org/protobuf v1.28.0
 )
@@ -11,6 +12,6 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
 	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
-	golang.org/x/text v0.3.3 // indirect
+	golang.org/x/text v0.3.5 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 )
